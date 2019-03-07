743b8cc7aa77c95e007f1b65d0d86baa8f594c51
uchansansan <uchansansan@gmail.com>
2019-03-07 10:47:44 +0700

    Update simulator2.cpp

--- simulator2.cpp
+++ simulator2.cpp
@@ -10,6 +10,9 @@ int main(){
     scanf("%d%d%d%d",&a,&b,&kol_ants,&kol_shagov);
     printf("%d %d %d %d\n",a,b,kol_ants,kol_shagov);
     int Pole[a][b], Ants[kol_ants][4];
+    FILE *out1,*out2;
+    out1 = fopen ("yshel.txt", "w+");
+    out2 = fopen("hodi.txt", "w+");
     for(int i = 0; i < a; i++){
         for(int j = 0; j < b; j++) scanf("%d",&Pole[i][j]);
         scanf("\n");
@@ -29,7 +32,6 @@ int main(){
     }
     cout<<'\n';
     for(int i = 0; i < kol_shagov; i++){
-    cout<<i<<" shag"<<'\n';
         for(int j = 0; j < kol_ants, Ants[j][3] == 1; j++){
             Pole[Ants[j][0]][Ants[j][1]] -= 2;
             if(Pole[Ants[j][0]][Ants[j][1]] <= 1 and i != 0){
@@ -54,15 +56,21 @@ int main(){
                 if(Pole[Ants[j][0]][Ants[j][1]] == 3) Pole[Ants[j][0]][Ants[j][1]] = 0;
                 if(Pole[Ants[j][0]][Ants[j][1]] == 2) Pole[Ants[j][0]][Ants[j][1]] = 1;
                 if(Pole[Ants[j][0]][Ants[j][1]] > 3) Pole[Ants[j][0]][Ants[j][1]] -= 2;
-                freopen ("yshel.txt", "w+", stdout);
-                printf("%d %d",i,j+1);
-                fclose(stdout);
+                //freopen ("yshel.txt", "w+", stdout);
+                fprintf(out1, "%d %d",i,j+1);
+               // fclose(stdout);
             }
         }
+        //freopen("hodi.txt", "w+", stdout);
+        fprintf(out2,"%d %s \n",i," shag");
         for(int i = 0; i < a; i++){
-            for(int j = 0; j < b; j++) cout<<Pole[i][j]<<" ";
-            cout<<'\n';
+            for(int j = 0; j < b; j++)  fprintf(out2,"%d ",Pole[i][j]);
+            fprintf(out2,"\n");
         }
-        cout<<'\n';
+        fprintf(out2,"\n");
     }
+
+    fclose(out1);
+    fclose(out2);
+
 }
